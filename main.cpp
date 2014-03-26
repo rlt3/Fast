ce9b7a9e4a61734c73ef65a63725aea1d8d0d5d4
rlt3 <leroy@bythehilt.com>
2014-03-25 21:30:08 -0400

    Filling in part of shape based on triangles inside it

--- main.cpp
+++ main.cpp
@@ -67,6 +67,10 @@ int main(int argc, char **argv) {
     int right  = polygon[0].x;
     int bottom = polygon[0].y;
     int left   = polygon[0].x;
+
+    float center_x;
+    float center_y;
+
     int i; 
 
     /* Find outer most points for all cardinal directions */
@@ -89,6 +93,14 @@ int main(int argc, char **argv) {
     }
 
     /* Set color to blue */
+    SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
+
+    center_x = polygon[0].x;
+    center_y = polygon[3].y;
+
+    SDL_RenderDrawPoint(render, center_x, center_y);
+
+    /* Set color to blue */
     SDL_SetRenderDrawColor(render, 0, 0, 255, 255);
 
     /* Draw the bounding box */
@@ -98,8 +110,10 @@ int main(int argc, char **argv) {
     /* Set color to red */
     SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
 
-    float x, y, m, b, my, mx;
-    int last;
+    float x,  y;            /* Graph coordinates & polygon coordinates */
+    float m,  b,  my,  mx;  /* Current  Slope intercept variables */
+    int   last;
+    int   next;
 
     /* 
      * Equation of line is always: y = mx+b. X & Y are the coordinate pairs.
@@ -112,27 +126,60 @@ int main(int argc, char **argv) {
      * y in slope intercept form: y = mx - b
      */
 
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 1; i++) {
       /* Make sure all points get connected, so connect first and last */
       last = (i == 0 ? 3 : i-1);
       
       /* Do top and bottom of slope to keep the syntax more sane */
-      my = (float)(polygon[last].y - polygon[i].y);
-      mx = (float)(polygon[last].x - polygon[i].x);
+      my = (polygon[last].y - polygon[i].y);
+      mx = (polygon[last].x - polygon[i].x);
       m  = (float)(my/mx);
       b  = polygon[i].y - (m * polygon[i].x);
 
       if (once < 4) {
-        printf("%d. (%d, %d) -> (%d, %d) | y(%.2f)/x(%.2f) : %.2f\n", i,
+        printf("%d. Now [(%d, %d); (%d, %d)] b: %.2f, m: %.2f\n", i,
+            polygon[last].x, polygon[last].y,
             polygon[i].x, polygon[i].y,
-            polygon[i-1].x, polygon[i-1].y,
-            my, mx, m);
+            b, m);
+        printf("center: (%f, %f)\n", center_x, center_y);
         once++;
       }
 
-      for (x = left; x < right; x++) {
-        y = (m*x) + b;
+      /**
+       * We are filling our shape by splitting it into triangles. First we draw
+       * the line from points a -> b, where a and b are just i and i-1. Then for
+       * every point at x, get the y value using the slope-intercept form. Draw
+       * the point if it doesn't exceed the bounds of our shape.
+       *
+       * Then, to fill our triangle, we need to know the center point of the
+       * shape. The center point is the common coordinate comprised of x, y
+       * values of each pair. All of our triangles are right triangles.
+       *
+       * So, for (A.x, A.y) and (B.x, B.y) the third coordinate might be
+       * (A.x, B.y) if it falls inside the bounds of the shape. If all the pairs
+       * of coordinates share this common point, it is deemed the center of the
+       * shape.
+       *
+       * For each x point (going across) we find the corresponding y for that
+       * line. We then start at y and move downwards until we hit the limit for
+       * that specific triangle. Repeat this for x across until we have filled 
+       * in our triangle.
+       *
+       * Another, I think, more simpler way is to just get the opposite slope
+       * value and start filling in until y has reached the opposite slope for
+       * that X.
+       */
+
+      int oy;
+      for (x = left; x < center_x; x++) {
+        y  = (m*x) + b;
+        oy = -(m*x) + b;
+
         if (y > top && y < bottom) { /* top starts at 0 here */
+          SDL_RenderDrawPoint(render, x, y);
+        }
+
+        for (y; y < oy; y++) {
           SDL_RenderDrawPoint(render, x, y);
         }
       }
